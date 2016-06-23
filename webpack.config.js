925cab9b13b67d559bc6ae15a79d2a6566c12a22
mjoldic <mjoldic@capfundr.com>
2016-06-23 00:20:14 -0400

    updated webpack config to include production version of react

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,11 @@ var config = {
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
       compress: { warnings: false }
+    }),
+    new webpack.DefinePlugin({
+      "process.env": {
+         NODE_ENV: JSON.stringify("production")
+       }
     })
   ]
 };
