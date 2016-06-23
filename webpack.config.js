0967d6124bbfd61cefd2aa1c427a8866e9e8a0ae
mjoldic <mjoldic@capfundr.com>
2016-06-22 22:12:43 -0400

    added webpack, babel, react support

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'js');
+var APP_DIR = path.resolve(__dirname, 'js/src');
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel'
+      },
+      { test: /\.json$/, loader: "json-loader" }    
+    ]
+  }
+};
+
+module.exports = config;
